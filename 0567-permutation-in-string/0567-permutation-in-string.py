2a6d90aa0e634cd13881fd00d1bbc6fa81dd8fab
Tushar Singh Bisht <aabisht2006@gmail.com>
2026-06-30 11:18:51 +0530

    0567-PERMUTATION-IN-STRING Stats: Time: 13 ms (85.29%), Space: 19.5 MB (18.09%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -3,18 +3,22 @@ class Solution:
         if len(s1) > len(s2):
             return False
             
-        count1 = [0]*26
+        count1 = {}
 
         for c in s1:
-            count1[ord(c)-ord('a')] += 1
+            count1[c] = count1.get(c, 0) + 1
+
+        count2 =  {}
+        for c in s2[0:len(s1)]:
+            count2[c] = count2.get(c, 0) + 1
 
         for l in range(0, len(s2)-len(s1)+1):
-            r = l + len(s1)
-            
-            count2 = [0]*26
-            for c in s2[l:r]:
-                count2[ord(c)-ord('a')] += 1
-            
+            r = l+len(s1)-1
+            if l > 0:
+                count2[s2[l-1]] -= 1
+                if count2[s2[l-1]] == 0:
+                    del count2[s2[l-1]]
+                count2[s2[r]] = count2.get(s2[r], 0) + 1
             if count1 == count2:
                 return True
 
