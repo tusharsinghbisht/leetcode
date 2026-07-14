64afe984ed31efb4825ae707d8b6f03393cb3245
Tushar Singh Bisht <aabisht2006@gmail.com>
2026-07-14 17:11:57 +0530

    0200-NUMBER-OF-ISLANDS Stats: Time: 239 ms (71.72%), Space: 21.8 MB (36.62%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -2,25 +2,40 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         m, n = len(grid), len(grid[0])
 
-        visited = set()
+        visited = [[False]*n for _ in range(m)]
 
-        def dfs(i,j):
-            if grid[i][j] != '1' or (i,j) in visited:
-                return
-            visited.add((i,j))
+        # def dfs(i,j):
+        #     if grid[i][j] != '1' or visited[i][j]:
+        #         return
+        #     visited[i][j] = True
             
-            moves = [(-1,0),(1,0),(0,1),(0,-1)]
-            for di, dj in moves:
-                ni, nj = i+di, j+dj
-                if ni >= 0 and ni < m and nj >= 0 and nj < n:
-                    dfs(ni, nj)
-                    
+        #     moves = [(-1,0),(1,0),(0,1),(0,-1)]
+        #     for di, dj in moves:
+        #         ni, nj = i+di, j+dj
+        #         if ni >= 0 and ni < m and nj >= 0 and nj < n:
+        #             dfs(ni, nj)
+        
+        def bfs(i, j):
+            queue = deque([(i, j)])
+            visited[i][j] = True
+            
+            while queue:
+                r, c = queue.popleft()
+                
+                moves = [(-1,0),(1,0),(0,1),(0,-1)]
+                for dr, dc in moves:
+                    nr, nc = r+dr, c+dc
 
+                    if nr >= 0 and nr < m and nc >= 0 and nc < n:
+                        if grid[nr][nc] == '1' and not visited[nr][nc]:
+                            queue.append((nr,nc))
+                            visited[nr][nc] = True
         count = 0
         for i in range(m):
             for j in range(n):
-                if grid[i][j] == '1' and not (i,j) in visited:
+                if grid[i][j] == '1' and not visited[i][j]:
                     count += 1
-                    dfs(i, j)
+                    # dfs(i, j)
+                    bfs(i, j)
 
         return count
