2adbde5a0585b803582cce9b36541c43f42434ff
Tushar Singh Bisht <aabisht2006@gmail.com>
2026-07-14 19:09:03 +0530

    0733-FLOOD-FILL Stats: Time: 3 ms (14.55%), Space: 19.7 MB (31.88%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,14 +1,12 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         m, n = len(image), len(image[0])
-        visited = [[False]*n for _ in range(m)]
         target = image[sr][sc]
 
+        if target == color:
+            return image
+
         def dfs(i, j):
-            if visited[i][j]:
-                return 
-            
-            visited[i][j] = True
             image[i][j] = color
             moves = [(-1,0),(1,0),(0,1),(0,-1)]
             for di, dj in moves:
@@ -23,15 +21,14 @@ class Solution:
             while queue:
                 i, j = queue.popleft()
                 image[i][j] = color
-                visited[i][j] = True
                 moves = [(-1,0),(1,0),(0,1),(0,-1)]
                 for di, dj in moves:
                     ni, nj = i + di, j+dj
                     if ni >= 0 and ni < m and nj >= 0 and nj < n:
-                        if image[ni][nj] == target and not visited[ni][nj]:
+                        if image[ni][nj] == target:
                             queue.append((ni, nj))
 
         # dfs(sr, sc)
-        bfs(sr, sc)
+        dfs(sr, sc)
 
         return image
