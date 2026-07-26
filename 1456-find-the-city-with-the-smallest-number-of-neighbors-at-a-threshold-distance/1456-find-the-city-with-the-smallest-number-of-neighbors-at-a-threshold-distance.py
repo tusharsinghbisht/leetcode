5aa484edbb6bffd2831467809cc5ef0db26d94b5
Tushar Singh Bisht <aabisht2006@gmail.com>
2026-07-26 05:31:13 +0530

    1456-FIND-THE-CITY-WITH-THE-SMALLEST-NUMBER-OF-NEIGHBORS-AT-A-THRESHOLD-DISTANCE Stats: Time: 91 ms (95.16%), Space: 20.9 MB (10.86%) - LeetHub

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
@@ -1,27 +1,51 @@
 class Solution:
     def findTheCity(self, n: int, edges: List[List[int]], threshold: int) -> int:
         # by floyd warshal algo, multi source shortest path algo
+        # mat = [[float("inf")]*n for _ in range(n)]
+        # for i in range(n):
+        #     mat[i][i] = 0
+
+        # for u, v, w in edges:
+        #     mat[u][v] = w
+        #     mat[v][u] = w
+
+        # for via in range(n):
+        #     for u in range(n):
+        #         for v in range(n):
+        #             mat[u][v] = min(
+        #                 mat[u][v],
+        #                 mat[u][via] + mat[via][v]
+        #             )
+
+        # by dijkstra
+        adj = defaultdict(list)
+        for u, v, t in edges:
+            adj[u].append((v,t))
+            adj[v].append((u,t))
+
         mat = [[float("inf")]*n for _ in range(n)]
-        for i in range(n):
-            mat[i][i] = 0
-
-        for u, v, w in edges:
-            mat[u][v] = w
-            mat[v][u] = w
-
-        for via in range(n):
-            for u in range(n):
-                for v in range(n):
-                    mat[u][v] = min(
-                        mat[u][v],
-                        mat[u][via] + mat[via][v]
-                    )
                     
+        for i in range(n):
+            pq = []
+            dist = mat[i]
+            dist[i] = 0
+            heappush(pq, (0, i))
+
+            while pq:
+                dis, node = heappop(pq)
+
+                if dis > dist[node]:
+                    continue
+                
+                for adjNode, edgeWt in adj[node]:
+                    if dist[adjNode] > dis + edgeWt:
+                        dist[adjNode] = dis + edgeWt
+                        heappush(pq, (dist[adjNode], adjNode))
+
         numCities = float("inf")
         res = -1
         for i in range(n):
             reachable = len([mat[i][j] for j in range(n) if i != j and mat[i][j] <= threshold])
-            print(i, reachable)
             if reachable <= numCities:
                 res = i
                 numCities = reachable
